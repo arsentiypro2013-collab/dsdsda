8195cfa7b6005b3c4e63163625327c2939e7ea7a
arsentiypro2013-collab <arsentiypro2013@gmail.com>
2026-01-16 19:32:36 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+import socket
+import tkinter as tk
+from threading import Thread
+from datetime import datetime
+
+SERVER_HOST = '194.67.206.24'  # Публичный IP хоста
+SERVER_PORT = 5002
+NAME = input("Твоё имя: ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((SERVER_HOST, SERVER_PORT))
+client.send(NAME.encode())
+
+root = tk.Tk()
+root.title("Чат - Клиент")
+messages = tk.Text(root, height=20, width=60)
+messages.pack(pady=10)
+entry = tk.Entry(root, width=50)
+entry.pack(pady=5)
+entry.focus()
+entry.bind('<Return>', lambda e: send_msg())
+
+def send_msg():
+    msg = entry.get()
+    if msg:
+        client.send(msg.encode())
+        entry.delete(0, tk.END)
+
+def receive():
+    while True:
+        try:
+            msg = client.recv(1024).decode()
+            messages.insert(tk.END, msg + "\n")
+            messages.see(tk.END)
+        except:
+            root.after(0, root.quit)
+            break
+
+Thread(target=receive, daemon=True).start()
+root.protocol("WM_DELETE_WINDOW", lambda: client.close() or root.quit())
+root.mainloop()
